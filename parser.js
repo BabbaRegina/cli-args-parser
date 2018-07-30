ca86f4b10c45c5b6e3623fc836c4cace39fb613f
WIN-T5MJGKKJ3NL\irene <irene.nicolini@gmail.com>
2018-07-30 23:45:16 +0200

    punti 1,2,3,4

---
+++ parser.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let args = process.argv.slice(2);
+console.log(' ARGS:', args);
+let goodInputs = [];
+
+for(let index in args){
+    const arg = args[index];
+    const isKey =  arg.length > 2 && arg.substring(0,2) === '--';
+    if (isKey) {
+        const keyLabel = arg.substring(2,arg.length).toString();
+        let input = {};
+        input[keyLabel] = true;
+        if (index < args.length) {
+            const next = args[++index];
+            const nextIsValue = next && next.length > 0 && next.substring(0,1) != '-';
+            if(nextIsValue) input[keyLabel] = +next? +next : next;
+        }
+        goodInputs.push(input);
+    }
+}
+console.log('GOOD INPUTS', goodInputs);
